5daf2eb85281920393c3b43ba83acb474c0f193b
meridacindy98 <meridacindy98@yahoo.com.ar>
2025-08-21 22:42:50 -0300

    first commit

---
+++ src/test/kotlin/com/example/challengeCupon/challengeCupon/ChallengeCuponApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.challengeCupon.challengeCupon
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChallengeCuponApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
